5cd0f58787e053c2ade3b227273bc848941a3d32
kawaiies <buyfme@gmail.com>
2026-06-20 17:34:42 +0800

    Delete lib directory

--- lib/get-slug-name.ts
+++
@@ -1,6 +0,0 @@
-// Remove spaces with - and always lowercase
-// returned by Algolia
-// 'AR optics' => 'ar-optics'
-const getSlugName = (path: string) => String(path).replace(/\s/g, '').toLocaleLowerCase();
-
-export default getSlugName;
